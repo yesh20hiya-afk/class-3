1075de609f491c8327dd00edc0e9ee2e5cf397eb
yesh20hiya-afk <yesh20hiya@gmail.com>
2025-12-17 20:29:24 -0500

    v2

---
+++ activity2.py
@@ -0,0 +1,23 @@
+#assigning different variables
+name="penguin"
+age=15
+is_student=True
+weight=38.5
+
+print("name:",name)
+print("Data type of name is:",type(name))
+
+print("age:",age)
+print("Data type of age is:",type(age))
+
+print("is_student:",is_student)
+print("Data type of is_student is:",type(is_student))
+
+print("weight:",weight)
+print("Data type of weight is:",type(weight))
+
+age=str(age)
+print(type(age))
+
+weight=int(weight)
+print(type(weight))
